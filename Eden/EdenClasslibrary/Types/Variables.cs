96f4846abd3ebd7d475887e2a2fd6a982829582e
Dariusz Krawczyk <darek9krawczyk@hotmail.com>
2025-02-25 21:05:40 +0100

    Lexer changes:   - Parsing files implemented   - Tests   - Float tokenization   - Lexer refactorization

---
+++ Eden/EdenClasslibrary/Types/Variables.cs
@@ -0,0 +1,18 @@
+﻿namespace EdenClasslibrary.Types
+{
+    public static class Variables
+    {
+        public static string[] Types
+        {
+            get
+            {
+                return new string[] { "Int", "Float", "String", "Bool" };
+            }
+        }
+
+        public static bool IsVariableType(string literal)
+        {
+            return Types.Contains(literal);
+        }
+    }
+}
---
+++ Eden/EdenTests/LexerTests/Int.cs
@@ -0,0 +1,90 @@
+﻿using EdenClasslibrary.Types;
+
+namespace EdenTests.LexerTests
+{
+    public class Int
+    {
+
+        [Fact]
+        public void ParseInt_1()
+        {
+            string code = $"202020;";
+            Lexer lexer = new Lexer();
+            lexer.SetInput(code);
+
+            List<Token> expected = new List<Token>()
+            {
+                new Token(TokenType.Int, "202020"),
+                new Token(TokenType.Semicolon, ";"),
+                new Token(TokenType.Eof, "\0"),
+            };
+
+            List<Token> actual = lexer.Tokenize().ToList();
+
+            Assert.Equal(expected.Count, actual.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Token expectedToken = expected[i];
+                Token actualToken = actual[i];
+
+                Assert.Equal(expectedToken.Keyword, actualToken.Keyword);
+                Assert.Equal(expectedToken.LiteralValue, actualToken.LiteralValue);
+            }
+        }
+
+        [Fact]
+        public void ParseInt_2()
+        {
+            string code = $"0;";
+            Lexer lexer = new Lexer();
+            lexer.SetInput(code);
+
+            List<Token> expected = new List<Token>()
+            {
+                new Token(TokenType.Int, "0"),
+                new Token(TokenType.Semicolon, ";"),
+                new Token(TokenType.Eof, "\0"),
+            };
+
+            List<Token> actual = lexer.Tokenize().ToList();
+
+            Assert.Equal(expected.Count, actual.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Token expectedToken = expected[i];
+                Token actualToken = actual[i];
+
+                Assert.Equal(expectedToken.Keyword, actualToken.Keyword);
+                Assert.Equal(expectedToken.LiteralValue, actualToken.LiteralValue);
+            }
+        }
+
+        [Fact]
+        public void ParseInt_3()
+        {
+            string code = $"0a;";
+            Lexer lexer = new Lexer();
+            lexer.SetInput(code);
+
+            List<Token> expected = new List<Token>()
+            {
+                new Token(TokenType.Int, "0"),
+                new Token(TokenType.Identifier, "a"),
+                new Token(TokenType.Semicolon, ";"),
+                new Token(TokenType.Eof, "\0"),
+            };
+
+            List<Token> actual = lexer.Tokenize().ToList();
+
+            Assert.Equal(expected.Count, actual.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Token expectedToken = expected[i];
+                Token actualToken = actual[i];
+
+                Assert.Equal(expectedToken.Keyword, actualToken.Keyword);
+                Assert.Equal(expectedToken.LiteralValue, actualToken.LiteralValue);
+            }
+        }
+    }
+}
